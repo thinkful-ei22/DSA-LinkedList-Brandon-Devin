e0fd8d471e57cacfba312acaa24a1a99c06d0903
dghero <devingreyhero@gmail.com>
2018-08-30 11:33:38 -0700

    Complete supplemental functions

--- sll-drills.js
+++ sll-drills.js
@@ -1,12 +1,8 @@
 'use strict';
 const SLL = require('./sll');
 
-
-
-
 function main (){
   const sll = new SLL();
-
   // Apollo, Boomer, Helo, Husker, Starbuck
 
   // sll.insertFirst('Apollo');
@@ -42,6 +38,19 @@ function main (){
   sll.remove('tauhida');
   display(sll);
 
+  console.log(getSllSize(sll));
+
+  console.log(isEmpty(sll));
+  
+  const sll2 = new SLL();
+  console.log(isEmpty(sll2));
+
+  console.log(findPrevious(sll, 'Husker'));
+
+  console.log(findLast(sll));
+  console.log(findLast(sll2));
+
+
 }
 
 function display (linkedList){
@@ -54,9 +63,49 @@ function display (linkedList){
 
     curNode = curNode.next;
   }
-
-
 }
+
+function getSllSize(linkedList){
+  let counter = 0;
+  let curNode = linkedList.head;
+
+  while(curNode !== null){
+    curNode = curNode.next;
+    counter++;
+  }
+  
+  return counter;
+}
+
+function isEmpty(linkedList){
+  return linkedList.head === null;
+}
+
+function findPrevious(linkedList, targetVal){
+  let curNode = linkedList.head;
+
+  while(curNode !== null && curNode.next !== null){
+    if(curNode.next.value === targetVal)
+      return curNode;
+    curNode = curNode.next;
+  }
+
+  console.log('Linked List: Could not find previous node');
+  return null;
+}
+
+function findLast(linkedList){
+  let curNode = linkedList.head;
+
+  if(curNode === null)
+    return curNode; //empty list
+
+  while(curNode.next !== null)
+    curNode = curNode.next;
+
+  return curNode;
+}
+
 
 main();
 
