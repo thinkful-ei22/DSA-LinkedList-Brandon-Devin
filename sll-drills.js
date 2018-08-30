2534300915a5efde2b506aaef47dc0a4194106f2
Brandon Graham <bgraham452@gmail.com>
2018-08-30 15:28:51 -0400

    added reverseList and getThirdFromEnd functions

--- sll-drills.js
+++ sll-drills.js
@@ -38,19 +38,24 @@ function main (){
   sll.remove('tauhida');
   display(sll);
 
-  console.log(getSllSize(sll));
+  // console.log(getSllSize(sll));
 
-  console.log(isEmpty(sll));
+  // console.log(isEmpty(sll));
   
-  const sll2 = new SLL();
-  console.log(isEmpty(sll2));
+  // const sll2 = new SLL();
+  // console.log(isEmpty(sll2));
 
-  console.log(findPrevious(sll, 'Husker'));
+  // console.log(findPrevious(sll, 'Husker'));
 
-  console.log(findLast(sll));
-  console.log(findLast(sll2));
+  // console.log(findLast(sll));
+  // console.log(findLast(sll2));
 
 
+  // console.log('Reverse List:');
+  // reverseList(sll);
+  // display(sll);
+
+  console.log('3rd to last node: ', thirdFromTheEnd(sll));
 }
 
 function display (linkedList){
@@ -112,6 +117,55 @@ function findLast(linkedList){
  * 
  *    Runtime: Polynomial, O(n^2)
  */
+
+
+function reverseList(linkedList){
+  let curNode = linkedList.head;
+  let prevNode = null, nextNode;
+
+  if (curNode === null){
+    return curNode;
+  }
+
+  while (curNode.next !== null){
+   
+
+    nextNode = curNode.next;
+    curNode.next = prevNode;
+    prevNode = curNode;
+    
+   
+    curNode = nextNode;
+
+
+  }
+  linkedList.head = curNode;
+  curNode.next = prevNode;
+
+  return linkedList;
+}
+
+function thirdFromTheEnd (linkedList){
+  let curNode = linkedList.head;
+  let prevTwoNode;
+  if (curNode === null || curNode.next === null || curNode.next.next === null){
+    return curNode;
+  }
+  prevTwoNode = curNode;
+  curNode = curNode.next.next;
+
+  while (curNode.next !== null)
+  {
+
+    curNode = curNode.next;
+    prevTwoNode = prevTwoNode.next;
+
+  }
+
+  return prevTwoNode;
+}
+
+
 
 main();
 
